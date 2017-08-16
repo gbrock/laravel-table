96e1bf9a39d1c71245e0e0e5f8bcc5ab8f98ae49
Bryn Jones <bryn.jones@reckless.agency>
2017-08-16 14:10:14 +0100

    Add ability to have other allowed parameters in appendPaginationLinks()
    Signed-off-by: Bryn Jones <bryn.jones@reckless.agency>

--- src/Table.php
+++ src/Table.php
@@ -219,9 +219,9 @@ class Table
     private function appendPaginationLinks()
     {
         if (class_basename($this->models) == 'LengthAwarePaginator') {
+            $allowed_parameters = array_merge([config('gbrock-tables.key_field'), config('gbrock-tables.key_direction')], config('gbrock-tables.allowed_parameters'));
             // This set of models was paginated.  Make it append our current view variables.
-            $this->models->appends(Input::only(config('gbrock-tables.key_field'),
-                config('gbrock-tables.key_direction')));
+            $this->models->appends(Input::only($allowed_parameters));
         }
     }
 }
